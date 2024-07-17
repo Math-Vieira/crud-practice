045c7653c85bb20d77853c4db79c1de048dd2fa2
math-vieira <matheusvieira9153@gmail.com>
2024-07-16 21:08:15 -0300

    refactor: update services templates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,12 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended'
+  ],
   ignorePatterns: ['dist', '.eslintrc.cjs', '.lintstagedrc.js'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
@@ -9,6 +14,7 @@ module.exports = {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true }
-    ]
+    ],
+    '@typescript-eslint/no-explicit-any': 'warn'
   }
 };
