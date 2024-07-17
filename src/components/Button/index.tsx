ed2eaff5bd27402c6b6e6dd201efa8a8de042d24
math-vieira <matheusvieira9153@gmail.com>
2024-07-17 14:44:41 -0300

    fix: fix Button component bug

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,6 +3,7 @@ import * as S from './styles';
 type Props = {
   children: string;
   buttonStyle?: 'default' | 'outline';
+  type?: 'button' | 'submit' | 'reset';
   loading?: boolean;
   disabled?: boolean;
 } & React.HTMLProps<HTMLButtonElement>;
@@ -11,13 +12,14 @@ export const Button = ({
   children = 'Button',
   buttonStyle = 'default',
   loading = false,
-  disabled = false
+  disabled = false,
+  type = 'button'
 }: Props) => {
   const Wrapper =
     buttonStyle === 'default' ? S.WrapperDefault : S.WrapperOutline;
 
   return (
-    <Wrapper disabled={disabled || loading} $loading={!!loading}>
+    <Wrapper disabled={disabled || loading} $loading={!!loading} type={type}>
       {children}
     </Wrapper>
   );
