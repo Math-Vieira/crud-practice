6084587b5683d22e8f1fce3374123dc22fae1883
math-vieira <matheusvieira9153@gmail.com>
2024-07-17 17:31:58 -0300

    feat(ui): add NotFound component

---
+++ src/components/NotFound/NotFound.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { NotFoundBody } from '.';
+
+const title = 'svg/NotFoundBody';
+
+export default {
+  title,
+  component: NotFoundBody
+} as Meta;
+
+export const Default: StoryObj = {
+  args: {}
+};
