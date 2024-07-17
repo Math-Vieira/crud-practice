130317a88b88cc5930d519d9f5cc66ef63a27ec7
math-vieira <matheusvieira9153@gmail.com>
2024-07-17 11:39:40 -0300

    feat(ui): add action button component

---
+++ src/components/ActionButton/ActionButton.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { ActionButton } from '.';
+
+const title = 'atoms/ActionButton';
+
+export default {
+  title,
+  component: ActionButton
+} as Meta;
+
+export const Default: StoryObj = {
+  args: {}
+};
---
+++ src/components/ActionButton/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles';
+
+export const ActionButton = () => {
+  return (
+    <S.Wrapper>
+      <h1>ActionButton</h1>
+    </S.Wrapper>
+  );
+};
