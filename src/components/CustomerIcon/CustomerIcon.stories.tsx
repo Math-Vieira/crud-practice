dc1a8f54dacac428a615b7798bb626370bce647c
math-vieira <matheusvieira9153@gmail.com>
2024-07-17 01:56:26 -0300

    feat(ui): add CustomerIcon component

---
+++ src/components/CustomerIcon/CustomerIcon.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { CustomerIcon } from '.';
+
+const title = 'svg/CustomerIcon';
+
+export default {
+  title,
+  component: CustomerIcon
+} as Meta;
+
+export const Default: StoryObj = {
+  args: {}
+};
